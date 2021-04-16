82615202b083f3b8e01fd64813fde5333fa1c78f
isolveditalready <isolveditalready@gmail.com>
2021-04-16 04:06:24 -0400

    sdf

--- alog.go
+++ alog.go
@@ -29,20 +29,31 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 
-	msgCh := make(chan string)
-	errorCh := make(chan error)
-
 	return &Alog{
-		dest:    w,
-		msgCh:   msgCh,
-		errorCh: errorCh,
+		dest:               w,
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		shutdownCh:         make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-
+	wg := &sync.WaitGroup{}
+loop:
+	for {
+		select {
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			go al.write(msg, nil)
+		case <-al.shutdownCh:
+			wg.Wait()
+			al.shutdown()
+			break loop
+		}
+	}
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -53,15 +64,26 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	al.m.Lock()
+	defer al.m.Unlock()
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	if err != nil {
+		go func(err error) {
+			al.errorCh <- err
+		}(err)
+	}
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
 func (al Alog) MessageChannel() chan<- string {
 
-	return nil
+	return al.msgCh
 }
 
 // ErrorChannel returns a channel that will be populated when an error is raised during a write operation.
@@ -74,6 +96,8 @@ func (al Alog) ErrorChannel() chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	<-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
